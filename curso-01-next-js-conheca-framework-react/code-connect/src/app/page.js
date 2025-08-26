0b6e8a22a0b603b82f78c2f22ba566dda0df27cf
Gabriel Frameschi <gabrieldframeschi@gmail.com>
2025-08-26 20:54:54 -0300

    curso 01: aula 04

--- curso-01-next-js-conheca-framework-react/code-connect/src/app/page.js
+++ curso-01-next-js-conheca-framework-react/code-connect/src/app/page.js
@@ -1,9 +1,10 @@
 import { CardPost } from "@/components/cardpost";
 import logger from "@/logger";
+import Link from "next/link";
 import styles from "./page.module.css";
 
-async function getAllPosts() {
-  const url = "http://localhost:3042/posts";
+async function getAllPosts(page) {
+  const url = `http://localhost:3042/posts?_page=${page}&_per_page=6`;
 
   try {
     const response = await fetch(url);
@@ -18,14 +19,20 @@ async function getAllPosts() {
   }
 }
 
-export default async function Home() {
-  const posts = await getAllPosts();
+export default async function Home({ searchParams }) {
+  const currentPage = searchParams?.page || 1;
+  const { data: posts, prev, next } = await getAllPosts(currentPage);
 
   return (
     <main className={styles.posts}>
       {posts.map((post) => (
         <CardPost key={post.id} post={post} />
       ))}
+
+      <div className={styles.pagination}>
+        {prev && <Link href={`/?page=${prev}`}>Página anterior</Link>}
+        {next && <Link href={`/?page=${next}`}>Próxima página</Link>}
+      </div>
     </main>
   );
 }
